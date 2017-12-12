89bd2967c66591e48b0ea27db550f55d3277c62a
cwy007 <chanweiyan007@gmail.com>
2017-12-13 01:23:36 +0800

    update frontend ui according to user login state

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -4,22 +4,27 @@ class ParkingsController < ApplicationController
     @parking = Parking.new
   end
 
+  def show
+    @parking = Parking.find(params[:id])
+  end
+
   def create
-    @parking = Parking.new( :parking_type => "guest", :start_at => Time.now )
+    @parking = Parking.new( :start_at => Time.now )
+    if current_user
+      @parking.parking_type = params[:parking][:parking_type]
+      @parking.user = current_user
+    else
+      @parking.parking_type = 'guest'
+    end
     @parking.save!
 
     redirect_to parking_path(@parking)
-  end
-
-  def show
-    @parking = Parking.find(params[:id])
   end
 
   def update
     @parking = Parking.find(params[:id])
     @parking.end_at = Time.now
     @parking.calculate_amount
-
     @parking.save!
 
     redirect_to parking_path(@parking)
