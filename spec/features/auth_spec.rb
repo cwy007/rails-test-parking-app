6984446918d9fe266f5690a56b2556fc85029939
cwy007 <chanweiyan007@gmail.com>
2017-12-13 07:20:07 +0800

    test login and logout process

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 # 4. 点击注册按钮
 # 5. 检查文字
 # 6. 检查资料有没有存进资料库
-feature 'register and login', :type => :feature do
+feature 'register, login and logout', :type => :feature do
   scenario 'register' do
     visit '/users/sign_up'
     expect(page).to have_content('Sign up')
@@ -19,5 +19,18 @@ feature 'register and login', :type => :feature do
     expect(page).to have_content('Welcome! You have signed up successfully.')
     user = User.last
     expect(user.email).to eq('foobar@example.com')
+  end
+
+  scenario 'login and logout' do
+    user = User.create!( :email => "foobar@example.com", :password => '12345678' )
+    visit '/users/sign_in'
+    within('#new_user') do
+      fill_in 'Email',    with: 'foobar@example.com'
+      fill_in 'Password', with: '12345678'
+    end
+    click_button 'Log in'
+    expect(page).to have_content('Signed in successfully')
+    click_link '登出'
+    expect(page).to have_content('Signed out successfully')
   end
 end
